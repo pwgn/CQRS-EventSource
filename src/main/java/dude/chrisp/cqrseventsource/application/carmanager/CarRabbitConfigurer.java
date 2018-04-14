76e0636b05ea04d287ce03aeb86a9b72c4c27f32
Christoffer Persson <christoffer.g.persson@gmail.com>
2018-04-14 21:07:59 +0200

    init readmodel rabbit mq listener

--- src/main/java/dude/chrisp/cqrseventsource/application/carmanager/CarRabbitConfigurer.java
+++ src/main/java/dude/chrisp/cqrseventsource/application/carmanager/CarRabbitConfigurer.java
@@ -1,31 +1,17 @@
 package dude.chrisp.cqrseventsource.application.carmanager;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.context.annotation.Bean;
 
 @EnableRabbit
 public class CarRabbitConfigurer {
 
-    public static final String TOPIC_EXCHANGE_NAME = "carmanager-exchange";
-    public static final String QUEUE_NAME = "carmanager-queue";
-    public static final String ROUTING_KEY = "messages.car";
+    public static final String FANOUT_EXCHANGE_CAR_MANAGER = "exchange.fanout.carmanager";
+    public static final String QUEUE_CAR_MANAGER = "queue.carmanager";
 
     @Bean
-    public TopicExchange CarExchange() {
-        return new TopicExchange(TOPIC_EXCHANGE_NAME);
-    }
-
-    @Bean
-    public Queue carQueue() {
-        return new Queue(QUEUE_NAME);
-    }
-
-    @Bean
-    public Binding declareBindingCarManager(Queue queue, TopicExchange exchange) {
-        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+    public FanoutExchange CarExchange() {
+        return new FanoutExchange(FANOUT_EXCHANGE_CAR_MANAGER);
     }
 }
--- src/main/java/dude/chrisp/cqrseventsource/readmodel/carmanager/CarManagerReadModel.java
+++ src/main/java/dude/chrisp/cqrseventsource/readmodel/carmanager/CarManagerReadModel.java
@@ -1,12 +1,23 @@
 package dude.chrisp.cqrseventsource.readmodel.carmanager;
 
+import dude.chrisp.cqrseventsource.application.carmanager.CarRabbitConfigurer;
+import dude.chrisp.cqrseventsource.domain.carmanager.event.CarCheckedinEvent;
+import dude.chrisp.cqrseventsource.domain.carmanager.event.CarCheckedoutEvent;
+import dude.chrisp.cqrseventsource.domain.carmanager.event.CarCreatedEvent;
 import dude.chrisp.cqrseventsource.domain.carmanager.model.Car;
 import dude.chrisp.cqrseventsource.domain.carmanager.spi.CarRepository;
+import org.springframework.amqp.rabbit.annotation.*;
 import org.springframework.stereotype.Service;
 
 import java.util.stream.Stream;
 
 @Service
+@RabbitListener(id = "CarManagerReadModelListener",
+        bindings = @QueueBinding(
+                value = @Queue(value = CarRabbitConfigurer.QUEUE_CAR_MANAGER + ".CarCreatedEvent"),
+                exchange = @Exchange(value = CarRabbitConfigurer.FANOUT_EXCHANGE_CAR_MANAGER, type = "fanout")
+        )
+)
 public class CarManagerReadModel {
     private final CarRepository carRepository;
 
@@ -16,5 +27,25 @@ public class CarManagerReadModel {
 
     public Stream<Car> GetAllCars() {
         return this.carRepository.getCars();
+    }
+
+    @RabbitHandler
+    public void receiveMessage(CarCreatedEvent event) {
+        System.out.println("Received car created event");
+    }
+
+    @RabbitHandler
+    public void receiveMessage(CarCheckedinEvent event) {
+        System.out.println("Received car checkedin event");
+    }
+
+    @RabbitHandler
+    public void receiveMessage(CarCheckedoutEvent event) {
+        System.out.println("Received car checkedout event");
+    }
+
+    @RabbitHandler(isDefault = true)
+    public void receiveMessage(Object event) {
+        System.out.println("Received unhandled message");
     }
 }
